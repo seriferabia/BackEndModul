19d43d21f389d7182a2717e66f4d34498ef2e4e7
seriferabia <seriferabia@gmail.com>
2019-06-16 14:35:58 +0200

    adding accountancy and administration

---
+++ reflections/hospital/drhouse-administration/src/main/java/at/nacs/drhouseadministration/view/controller/InvoiceController.java
@@ -0,0 +1,39 @@
+package at.nacs.drhouseadministration.view.controller;
+
+import at.nacs.drhouseadministration.view.logic.InvoiceClient;
+import at.nacs.drhouseadministration.view.model.Invoice;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.security.RolesAllowed;
+import java.util.List;
+
+@Controller
+@RequestMapping("/invoices")
+@RequiredArgsConstructor
+public class InvoiceController {
+  private final InvoiceClient client;
+
+  @ModelAttribute("invoice")
+  Invoice invoice() {
+    return new Invoice();
+  }
+
+  @ModelAttribute("invoices")
+  List<Invoice> invoices() {
+    return client.getInvoices();
+  }
+
+  @GetMapping
+  String page() {
+    return "invoice";
+  }
+
+  @RolesAllowed("ACCOUNTANT")
+  @PostMapping("/paid")
+  String post(@RequestParam String id) {
+    client.markAsPaid(id);
+    return "redirect:/invoices";
+  }
+}
